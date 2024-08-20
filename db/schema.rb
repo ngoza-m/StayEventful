e919afced4a338f8d0ba1865d5d815253a2b5902
Ngoza Mayaka <ngozamay@gmail.com>
2024-08-20 18:10:51 +0200

    styling the home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_08_20_145114) do
-
-ActiveRecord::Schema[7.1].define(version: 2024_08_20_125754) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +33,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_20_125754) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.string "city"
     t.string "country"
-    t.string "image_url"
   end
 
 end
