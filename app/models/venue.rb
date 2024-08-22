5051a39f8b10beed19dcd7ecbae071a9aaa225d2
natasha tellez <natelz4@hotmail.com>
2024-08-22 17:14:01 +0200

    Add new footer and signup log in

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,9 +3,9 @@ class Venue < ApplicationRecord
 
   # validates :name, :address, :city, :country, :price, presence: true
   # validates :price, numericality: { greater_than_or_equal_to: 0 }
-  
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+
+  #geocoded_by :address
+  #after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
