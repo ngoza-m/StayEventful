6810d0d28a35da3669cf820ce6427527f017fd26
Ngoza Mayaka <ngozamay@gmail.com>
2024-08-23 16:33:18 +0200

    profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def profile
+    
     # @venues = Venue.all
     @venues = Venue.where(user_id: current_user.id)
+
     @bookings = Booking.where(user_id: current_user.id)
-    # @other_bookings = Booking.where()
+
+    # @other_bookings = Booking.where(venue_id: @venue.id)
   end
 
   def home
